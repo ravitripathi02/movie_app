fde6d5f90e74fe0bd42c1b96acce14048d581186
ravitripathi02 <91239605+ravitripathi02@users.noreply.github.com>
2023-03-05 16:33:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -100,6 +100,23 @@ function App() {
       setMovies(responseJson.Search);
     }
   };
+  const addFavouriteMovie = (movie) => {
+    if (favourites?.indexOf(movie) == -1) {
+      const newFavouriteList = [...favourites, movie];
+      setfavourites(newFavouriteList);
+      saveTolocalStorage(newFavouriteList);
+    } else {
+      setfavourites(favourites);
+      saveTolocalStorage(favourites);
+    }
+  };
+  const removeFavMovie = (movie) => {
+    const newFav = favourites.filter(
+      (favourite) => favourite.imdbID != movie.imdbID
+    );
+    setfavourites(newFav);
+    saveTolocalStorage(newFav);
+  };
   useEffect(() => {
     getMovieReq(searchValue);
   }, [searchValue]);
@@ -118,23 +135,7 @@ function App() {
     setTimeout(() => getMovieReq(event.target.value), 2000);
     updateTimeoutId(timeOutId);
   };
-  const addFavouriteMovie = (movie) => {
-    if (favourites?.indexOf(movie) == -1) {
-      const newFavouriteList = [...favourites, movie];
-      setfavourites(newFavouriteList);
-      saveTolocalStorage(newFavouriteList);
-    } else {
-      setfavourites(favourites);
-      saveTolocalStorage(favourites);
-    }
-  };
-  const removeFavMovie = (movie) => {
-    const newFav = favourites.filter(
-      (favourite) => favourite.imdbID != movie.imdbID
-    );
-    setfavourites(newFav);
-    saveTolocalStorage(newFav);
-  };
+  
 
   return (
     <>
